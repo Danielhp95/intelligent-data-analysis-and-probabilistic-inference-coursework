9b64ec072c0ce324beec84c22484954c15806fdb
Daniel Hernandez <dh1213@arc04.doc.ic.ac.uk>
2017-02-02 15:32:11 +0000

    added dep table

--- IDAPICoursework02.py
+++ IDAPICoursework02.py
@@ -48,58 +48,54 @@ def DependencyList(depMatrix):
             flattened_dep_matrix.append( (depMatrix[i][j], i, j) )
     # Cool python. indexing using [::2] gets every other element.
     depList = sorted(flattened_dep_matrix, key=lambda x: x[0], reverse=True)
-    return array(depList)
-#
-# Functions implementing the spanning tree algorithm
-# Coursework 2 task 4
+    return array(depList[::2])
 
 # Assumes that depList is already sorted
-def SpanningTreeAlgorithm(depList, noVariables):
-    spanningTree = []
+def SpanningTreeAlgorithm(depList):
+    spanningTree = {}
     threshold = 100
     highest_dependency, _, _ = depList[0]
 
     i = 0
-    current_depedency, n1, n2 = dep_matrix[i]
-    while highest_dependency > current_depedency * threshold:
+    current_depedency, n1, n2 = depList[i]
+    while current_depedency * threshold > highest_dependency:
+        n1, n2 = int(n1), int(n2)
         if not checkForLoop(spanningTree, n1, n2):
-            spanningTree.append([n1,n2])
+            addNode(spanningTree, n1, n2)
         i += 1
-        current_depedency, n1, n2 = dep_matrix[i]
+        current_depedency, n1, n2 = depList[i]
 
-    return array(spanningTree)
+    return spanningTree
+
+def addNode(spanningTree, n1, n2):
+    if not n1 in spanningTree.keys():
+        spanningTree[n1] = set()
+    if not n2 in spanningTree.keys():
+        spanningTree[n2] = set()
+    spanningTree[n1].add(n2)
+    spanningTree[n2].add(n1)
+    
+
+def checkForLoop(graph, start, end):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            if graph and vertex in graph.keys():
+                queue.extend(graph[vertex] - visited)
+    return end in visited
+    
 
 # Checks if adding node would create a loop. Performs breadth first traversal
-def checkForLoop(spanningTree, n1, n2):
-    breadth_queue = deque()
-    visited = [n1]
-    breadth_queue.append( (n1, n2) )
-    while len(breadth_queue) > 0:
-        _, node = breadth_queue.popleft()
-        visited.append(node) 
-        for p, c in spanningTree:
-            if p == node:
-                if c in visited:
-                    return True
-                else:
-                    breadth_queue.append( (node, c) )
-    return False
-
-
-
-
 
 # ----- UNCOMMENT FOR SUBMISSION -------- #
 numpy.set_printoptions(suppress=True) # In order without scientific notation
 
-noVariables, noRoots, noStates, noDataPoints, datain = ReadFile("HepatitisC.txt")
-theData    = [list(x) for x in array(datain)]
-noStates   = list(noStates)
-dep_matrix = DependencyMatrix(theData, noVariables, noStates)
-dep_list   = DependencyList(dep_matrix)
-#print(dep_list)
-
-#AppendString("results02.txt", "Group members: Daniel Hernandez Perez")
-#AppendString("results02.txt", "Dependency matrix for HepatitisC:")
-#AppendString("results02.txt", "")
-#AppendArray("results02.txt",)
+#noVariables, noRoots, noStates, noDataPoints, datain = ReadFile("HepatitisC.txt")
+#theData    = [list(x) for x in array(datain)]
+#noStates   = list(noStates)
+#dep_matrix = DependencyMatrix(theData, noVariables, noStates)
+#dep_list   = DependencyList(dep_matrix)
+#tree = SpanningTreeAlgorithm(dep_list)
+#print(tree)
