42628cccca638303eda4756bd0c12b44c19802ce
Daniel Hernandez <dh1213@arc04.doc.ic.ac.uk>
2017-02-02 12:51:38 +0000

    added spaning tree

--- IDAPICoursework02.py
+++ IDAPICoursework02.py
@@ -1,6 +1,7 @@
 from IDAPICourseworkLibrary import *
 from IDAPICourseworkSkeleton import *
 from numpy import *
+from collections import deque
 
 # Coursework 2 begins here
 #
@@ -25,16 +26,19 @@ def marginalise_probability(jpt):
 # construct a dependency matrix for all the variables
 def DependencyMatrix(theData, noVariables, noStates):
     MIMatrix = zeros((noVariables,noVariables))
-    # Diagonal elements should be 0 (?)
+    # Diagonal elements should be 0
     for i in range(len(MIMatrix)):
         for j in range(i+1, len(MIMatrix[0])):
-            mi = MutualInformation(JPT(theData, i, j, noStates))
-            # Symmetrical matrix. Dep(A,B) = Dep(B,A)
-            MIMatrix[i][j] = mi
-            MIMatrix[j][i] = mi
+            if i != j:
+                mi = MutualInformation(JPT(theData, i, j, noStates))
+                # Symmetrical matrix. Dep(A,B) = Dep(B,A)
+                MIMatrix[i][j] = mi
+                MIMatrix[j][i] = mi
+            else:
+                MIMatrix[i][i] = 0 
     return MIMatrix
 
-# Function to compute an ordered list of dependencies 
+# Function to compute an ordered list of dependencies
 def DependencyList(depMatrix):
     depList=[]
     flattened_dep_matrix = [] 
@@ -42,30 +46,58 @@ def DependencyList(depMatrix):
     for i in range(len(depMatrix)):
         for j in range(len(depMatrix[0])):
             flattened_dep_matrix.append( (depMatrix[i][j], i, j) )
+    # Cool python. indexing using [::2] gets every other element.
     depList = sorted(flattened_dep_matrix, key=lambda x: x[0], reverse=True)
     return array(depList)
 #
 # Functions implementing the spanning tree algorithm
 # Coursework 2 task 4
 
+# Assumes that depList is already sorted
 def SpanningTreeAlgorithm(depList, noVariables):
     spanningTree = []
-  
+    threshold = 100
+    highest_dependency, _, _ = depList[0]
+
+    i = 0
+    current_depedency, n1, n2 = dep_matrix[i]
+    while highest_dependency > current_depedency * threshold:
+        if not checkForLoop(spanningTree, n1, n2):
+            spanningTree.append([n1,n2])
+        i += 1
+        current_depedency, n1, n2 = dep_matrix[i]
+
     return array(spanningTree)
 
-#
-# End of coursework 2
-#
+# Checks if adding node would create a loop. Performs breadth first traversal
+def checkForLoop(spanningTree, n1, n2):
+    breadth_queue = deque()
+    visited = [n1]
+    breadth_queue.append( (n1, n2) )
+    while len(breadth_queue) > 0:
+        _, node = breadth_queue.popleft()
+        visited.append(node) 
+        for p, c in spanningTree:
+            if p == node:
+                if c in visited:
+                    return True
+                else:
+                    breadth_queue.append( (node, c) )
+    return False
+
+
+
+
 
 # ----- UNCOMMENT FOR SUBMISSION -------- #
-numpy.set_printoptions(suppress=True) # In order to print it nicely:
+numpy.set_printoptions(suppress=True) # In order without scientific notation
 
-#noVariables, noRoots, noStates, noDataPoints, datain = ReadFile("HepatitisC.txt")
-#theData    = [list(x) for x in array(datain)]
-#noStates   = list(noStates)
-#dep_matrix = DependencyMatrix(theData, noVariables, noStates)
-#dep_list   = DependencyList(dep_matrix)
-#print_lateX_dep_list
+noVariables, noRoots, noStates, noDataPoints, datain = ReadFile("HepatitisC.txt")
+theData    = [list(x) for x in array(datain)]
+noStates   = list(noStates)
+dep_matrix = DependencyMatrix(theData, noVariables, noStates)
+dep_list   = DependencyList(dep_matrix)
+#print(dep_list)
 
 #AppendString("results02.txt", "Group members: Daniel Hernandez Perez")
 #AppendString("results02.txt", "Dependency matrix for HepatitisC:")
